c67818408236c227be2777dd6895a13e34687f89
kurves <kavssimply@gmail.com>
2024-02-17 03:53:08 -0800

    function to sort array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,62 +14,22 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	listint_t *head, *current;
-	
-	head = NULL;
-	current = NULL;
 	
 	if (array == NULL || size < 2)
 	{
 		return;
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		current = malloc(sizeof(listint_t));
-		if (current == NULL)
+		for (j = 0; j < size - i - 1; j++)	
 		{
-			fprintf(stderr, "Memory allocation failed\n");
-			exit(EXIT_FAILURE);
-		}
-		current->n = array[i];
-		current->prev = NULL;
-		current->next = head;
-		if (head != NULL)
-		{
-			head->prev = current;
-			head = current;
-		}
-		for (i = 0; i < size - 1; i++)
-		{
-			for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					current = head;
-					while (current->next != NULL)
-					{
-						if (current->n == array[j])
-						{
-							current->n = array[j + 1];
-						}
-						else if (current->n == array[j + 1])
-						{
-							current->n = array[j];
-						}
-						current = current->next;
-					}
-					printf("Swap elements: ");
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
-		while (head != NULL)
-		{
-			current = head->next;
-			free(head);
-			head = current;
-		}
+	}
 }
