1d40b989036ebbeed381270c06a1bf66e2673694
kurves <kavssimply@gmail.com>
2024-02-17 03:56:25 -0800

    fix betty coding style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 #include "sort.h"
 
 /**
- * bubble-sort - sorting algoritm
+ * bubble_sort - sorting algoritm
  * @array: array of integers
  * @size: size of array
  * Description: algorithm to sort list and array contents
@@ -14,14 +14,14 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	
+
 	if (array == NULL || size < 2)
 	{
 		return;
 	}
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)	
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
