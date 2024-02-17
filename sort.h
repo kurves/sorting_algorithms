819881e836b182c93567b0f08c356d36866989e7
kurves <kavssimply@gmail.com>
2024-02-17 03:09:41 -0800

    add function prototypes

--- sort.h
+++ sort.h
@@ -4,5 +4,5 @@
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
 #endif
