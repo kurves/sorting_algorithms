63e4d7e2964ff765ddd2ce5c1b0c02229754b773
kurves <kavssimply@gmail.com>
2024-02-17 08:00:17 -0800

    quick algorith sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,37 +3,73 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
+ * lomuto_partition - Partitions the array using Lomuto partition scheme
+ * @array: Array of integers to be partitioned
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Number of elements in the array
+ * Description: function to sort array
+ * Return: Index of the pivot element after partitioning
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
+	
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+			{
+				print_array(array, size);
+			}
+		}
+	}
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	if (i + 1 != high)
+	{
+		print_array(array, size);
+	}
+	return i + 1;
+}
+
+/**
+ * quick_sort_recursive - Recursively sorts the array using Quick sort algorithm
+ * @array: Array of integers to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Number of elements in the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot_idx = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot_idx - 1, size);
+		quick_sort_recursive(array, pivot_idx + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order
  * @array: Array of integers to be sorted
  * @size: Number of elements in the array
  * Description: function to sort array
  * Return: nothing
- */
-void selection_sort(int *array, size_t size)
+*/ 
+void quick_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
-	int temp;
-
 	if (array == NULL || size < 2)
 	{
 		return;
 	}
-	for (i = 0; i < size - 1; i++)
-	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_idx])
-			{
-				min_idx = j;
-			}
-		}
-		if (min_idx != i)
-		{
-			temp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
-			print_array(array, size);
-		}
-	}
+	quick_sort_recursive(array, 0, size - 1, size);
 }
